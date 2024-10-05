ff869aa38c7ac14857e25e0491031c2120f9951e
CLENTAN CHAUKE <clentanchauke9089@gmail.com>
2024-10-05 09:35:53 +0200

    Update Logo.js

--- src/components/navbar/Logo.js
+++ src/components/navbar/Logo.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import Logo from '../navbar/logoo.jpg';
+import Logo from '../navbar/logoo.jpg'
 
 const User = ({ name, description, avatarSrc }) => {
   return (
-    <div className="flex items-center p-4 rounded-lg shadow-lg max-w-xs bg-bodyColor text-white">
-      {/* Avatar Image */}
+    <div className="flex items-center p-4 rounded-lg shadow-lg max-w-xs bg-bodyColor  text-white">
       <img
-        src={avatarSrc}
+        src={Logo}
         alt={`${name}'s avatar`}
         className="w-16 h-16 rounded-full mr-4 border-2 border-white"
       />
-      {/* Name and Description - Hidden on Mobile */}
-      <div className="hidden md:block">
+      <div>
         <h2 className="text-xl font-semibold">{name}</h2>
         <p className="text-gray-300">{description}</p>
-      </div>
-      {/* Show only name and description on larger screens */}
-      <div className="md:hidden">
-        <h2 className="text-xl font-semibold">{name}</h2>
       </div>
     </div>
   );
@@ -32,3 +26,4 @@ export default function App() {
     />
   );
 }
+
