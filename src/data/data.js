805b524d99fa2defb1e675a90c1312158f6a951b
CLENTAN CHAUKE <clentanchauke9089@gmail.com>
2024-10-05 09:04:07 +0200

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -7,36 +7,31 @@ export const featuresData = [
   {
     id: 1,
     title: "Business Stratagy",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "A business strategy is a long-term plan to achieve company goals and growth by analyzing the market, identifying a target audience, offering unique value, and optimizing operations. It focuses on staying competitive, managing finances, and driving innovation.",
   },
   {
     id: 2,
     icon: <AiFillAppstore />,
     title: "App Development",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "App development involves creating software for mobile or web platforms through design, coding, testing, and deployment. It focuses on building user-friendly, functional apps for platforms like Android or iOS, ensuring smooth performance, and providing regular updates for improvement.",
   },
   {
     id: 3,
     icon: <SiProgress />,
     title: "SEO Optimisation",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "Effective SEO boosts website rankings, visibility, and user engagement.",
   },
   {
     id: 4,
     icon: <FaMobile />,
     title: "Mobile Development",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "Mobile development combines design, coding, and testing to deliver functional and engaging mobile apps.",
   },
   {
     id: 5,
     icon: <SiAntdesign />,
     title: "UX Design",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
-  },
-  {
-    id: 6,
-    icon: <FaGlobe />,
-    title: "Hosting Websites",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
-  },
+    des: "The goal is to improve user satisfaction and create seamless, intuitive interactions.",
+  }
+
 ];
